62c61ceb5d9862a1c02e018c226ec2bf79b8a82a
dhsung <doohwannet7@naver.com>
2022-12-12 14:15:46 +0900

    Main Frame  정의
    Main row, Title bar 정의

---
+++ Manaview/UI/Converter/BoolToVisibilityConverter.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Manaview.UI.Converter
+{
+    class BoolToVisibilityConverter : IValueConverter
+    {
+        /// <summary>
+        /// WPF 에서 binding 되는 값을 변경하고자 할때
+        /// boolean 값으로 visible 값을 변경
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value!=null && value is bool)
+            {
+                if((bool)value != true)
+                {
+                    return Visibility.Collapsed;
+                }
+                else
+                {
+                    return Visibility.Visible;
+                }
+            }
+            else
+            {
+                return Visibility.Collapsed;
+            }
+        }
+
+        /// <summary>
+        /// 들어간 값에 대해서 control할때
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value != null && value is Visibility)
+            {
+                Visibility _visibility = (Visibility)value;
+                if(_visibility != Visibility.Visible)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+
+        
+    }
+}
