85ae89d4835bafda11f667728c42e0f84c720cbe
dhsung <doohwannet7@naver.com>
2022-12-19 15:41:03 +0900

    menubar 만들기
    open command

--- Manaview/UI/MainFrame/MenuBarView.xaml.cs
+++ Manaview/UI/MainFrame/MenuBarView.xaml.cs
@@ -13,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Manaview.UI.MainFrame
+namespace Manaview
 {
     /// <summary>
     /// MenuBar.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class MenuBar : UserControl
+    public partial class MenuBarView : UserControl
     {
-        public MenuBar()
+        public MenuBarView()
         {
             InitializeComponent();
         }
